1534338caaf7f7d1e420667a55be0e7613656a26
leen-neel <and24903@gmail.com>
2022-11-27 20:39:32 +0530

    added base css file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,21 @@ module.exports = {
         extend: {},
     },
     plugins: [require('daisyui')],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: '#d94948',
+                    secondary: '#4959ed',
+                    accent: '#38077c',
+                    neutral: '#1F2228',
+                    'base-100': '#1f2937',
+                    info: '#5eead4',
+                    success: '#126E52',
+                    warning: '#C49B08',
+                    error: '#FA1E29',
+                },
+            },
+        ],
+    },
 };
