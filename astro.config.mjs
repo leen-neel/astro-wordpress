20f880e39d968ff79b55591403bdce3a82cddf84
leen-neel <and24903@gmail.com>
2022-11-28 19:20:30 +0530

    switched to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+    integrations: [tailwind()],
+    output: 'static',
 });
